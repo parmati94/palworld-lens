d2dc3d90490eeb805bc8c74b63f75521a49e9903
Paul Armati <parmati94@gmail.com>
2025-12-31 14:50:24 -0500

    feat: split app.js into services; remove tailwind CDN & use proper tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./login.html",
+    "./js/**/*.js",
+    "./partials/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
